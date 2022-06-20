9ddc3624a5469ff9d8bcf280bc3628639a89f2b5
exploitenomah <iksploiting@gmail.com>
2022-06-20 19:20:36 +0100

    extract find model to middleware

--- server/middleware/modelMiddleware.js
+++ server/middleware/modelMiddleware.js
@@ -2,7 +2,8 @@
 const { asyncHelper } = require('../utils/asyncUtils')
 const { createCustomError } = require('../utils/controllerUtils')
 module.exports.handleNotFound = (Model) => asyncHelper(async (req, res, next) => {
-  const model = await Model.findOne({ _id: req.params.id })
+  const { id } = req.params.id
+  const model = await Model.findOne({ id: id })
   if (!model) return next(createCustomError('No document with that id!', 404))
   req.doc = model
   next()

dbd6f89904713e67d7fa6cd936c9d777c922f5ff
exploitenomah <iksploiting@gmail.com>
2022-07-10 18:20:10 +0100

    basic setup of material-ui

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
+import Typography from '@mui/material/Typography'
 
+ 
 const Home: NextPage = () => {
+  
   return (
     <div>
+      <Typography  >Hello Next.js</Typography>
+      Hello world 
     </div>
-  )
+  ) 
 }
 
 export default Home
+ 

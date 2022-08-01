a0813aff273e26bb58c5d37556364281827f19b0
exploitenomah <iksploiting@gmail.com>
2022-08-02 00:12:20 +0100

    refactor index and about e2e tests

--- client/cypress/e2e/about-us.cy.ts
+++ client/cypress/e2e/about-us.cy.ts
@@ -4,19 +4,26 @@ const baseUrl = '/about-us'
 
 describe('About us page is rendered with appropriate elements', () => {
 
+  before(() => {
+    cy.visit(baseUrl)
+  })
   it('renders a main element', () => {
-     cy.visit(baseUrl)
      cy.get('main').should('exist')
   })
 
   it('renders a heading', () => {
-    cy.visit(baseUrl)
     cy.get('h1').should('exist').should('contain', 'About Us')
   })
 
   it('renders call to action', () => {
-    cy.visit(baseUrl)
     cy.get('a[href="/careers"]').should('exist').should('contain.text', 'join our team')
+  })
+})
+
+describe('about call to action leads to careers page', () => {
+  it('navigates to careers page on click of call to action', () => {
+    cy.get('a[href="/careers"]').click()
+    cy.url().should('include', '/careers')
   })
 })
 
--- client/cypress/e2e/index.cy.ts
+++ client/cypress/e2e/index.cy.ts
@@ -3,29 +3,48 @@
 const baseUrl = '/';
 
 describe('Home page is rendered with appropriate elements', () => {
-  it('renders a main element', () => {
+  beforeEach(() => {
     cy.visit(baseUrl)
+  })
+
+  it('renders a main element', () => {
     cy.get('main').should('exist')
   })
+
   it('renders a heading', () => {
-    cy.visit(baseUrl)
     cy.get('h1').should('exist').should('contain', 'GoldenCity Model Management')
   })
+
   it('renders call to action', () => {
-    cy.visit(baseUrl)
     const callToAction = cy.get('[data-testid="hero-cta"]')
-    callToAction.children('a').its('length').should('eq', 2)
-    callToAction.get('a[href="/main-board"]').should('exist').should('contain.text', 'main board')
-    callToAction.get('a[href="/new-faces"]').should('exist').should('contain.text', 'new faces')
+    callToAction.children('a').should('have.length', 2)
+    callToAction.get('a[href="/main-board"]').should('exist')
+    callToAction.get('a[href="/new-faces"]').should('exist')
   }) 
   
   it('renders gallery section with heading and link', () => {
     const gallery = cy.get('[data-testid="gallery-preview"]')
     gallery.should('exist')
-    gallery.get('h2').should('exist').should('contain.text', 'Talent at its finest')
-    gallery.get('a[href="/gallery"]').should('exist').should('contain.text', 'explore gallery')
+    gallery.children('h2').should('exist').should('contain.text', 'Talent at its finest')
+    cy.get('a[href="/gallery"]').should('exist').should('contain.text', 'explore gallery')
+  })
+})
+
+describe('call to action leads to appropriate page', () => {
+  beforeEach(() => {
+    cy.visit(baseUrl)
+  })
+  it('navigates to main board page on click of call to action', () => {
+    const hero = cy.get('[data-testid="hero-cta"]')
+    hero.children('a[href="/main-board"]').click()
+    cy.url().should('include', '/main-board')
   })
 
+  it('navigates to new faces page on click of call to action', () => {
+    const hero = cy.get('[data-testid="hero-cta"]')
+    hero.children('a[href="/new-faces"]').click()
+    cy.url().should('include', '/new-faces')
+  })
 })
 
 // Prevent TypeScript from reading file as legacy script

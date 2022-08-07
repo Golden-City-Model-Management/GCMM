fdd857c9b5c5acaa313dd0fafe555467bb972b73
exploitenomah <iksploiting@gmail.com>
2022-08-07 15:55:36 +0100

    fix failing contact page test

--- client/cypress/e2e/contact-us.cy.ts
+++ client/cypress/e2e/contact-us.cy.ts
@@ -29,8 +29,8 @@ describe('contact form submits', () => {
   it('submits form', () => {
     cy.get('input[name="name"]').type('Test User')
     cy.get('input[name="email"]').type('test@email.com')
-    cy.get('input[name="message"]').type('This is a test message')
-    cy.get('input[type="submit"]').click()
+    cy.get('textarea[name="message"]').type('This is a test message')
+    cy.get('button[type="submit"]').click()
     const successAlert = cy.get('[data-testid="contact-form-success"]').should('exist').should('contain.text', 'Your message has been sent!')
     cy.get('[data-testid="contact-form-success"]').children('[data-testid="contact-form-success-close"]').click()
     successAlert.should('not.exist')

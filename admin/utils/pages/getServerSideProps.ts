a94479de8881a76895ae33c58694bb96c6d41224
exploitenomah <iksploiting@gmail.com>
2022-08-20 16:42:21 +0100

    fix failing test for login without form

--- admin/utils/pages/getServerSideProps.ts
+++ admin/utils/pages/getServerSideProps.ts
@@ -9,11 +9,15 @@ const getUserDetails: GetServerSideProps = async (ctx) => {
 
   const accessTokenCookie = ctx.req.cookies['access_token']
   const accessTokenFromHeader = ctx.req.headers.authorization.split(' ')[1]
-  if(!ctx.resolvedUrl.includes('/login') && (!accessTokenCookie || !accessTokenFromHeader)) {
+  const accessToken = accessTokenCookie || accessTokenFromHeader
+
+  if(!ctx.resolvedUrl.includes('/login')) {
+    if(!accessToken) {
     ctx.res.writeHead(302, {
       Location: '/admin/login'
     })
     ctx.res.end()
+   }
   }
 
   return {

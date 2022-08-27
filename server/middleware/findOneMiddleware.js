b7ed0150be697a1c5cec1e877b92519f28c4d9f6
exploitenomah <iksploiting@gmail.com>
2022-08-27 19:55:16 +0100

    create route to get model portfolio images

---
+++ server/middleware/findOneMiddleware.js
@@ -0,0 +1,17 @@
+
+const { asyncHelper } = require('../utils/asyncUtils')
+const { createCustomError } = require('../utils/controllerUtils')
+
+module.exports.findDocument = (Model) => asyncHelper(async (req, res, next) => {
+  const { id } = req.params.id
+  const queryKeys = Object.keys(req.query)
+  const queryObj = {}
+  queryKeys.forEach(key => {
+    queryObj[key] = req.query[key]
+  })
+  if(!id && queryKeys.length === 0) return next(createCustomError('Invalid id or id not defined! Use id or make query by name.', 400))
+  const doc = await (id ?  Model.findOne({ id: id }) : Model.findOne({...queryObj }))
+  if (!doc) return next(createCustomError('No document found', 404))
+  req.doc = doc
+  next()
+})

e95cb52665b1cd2a3e124ce165479201b98a4adc
exploitenomah <iksploiting@gmail.com>
2022-09-03 10:32:52 +0100

    test fix 1

--- admin/pages/models/index.tsx
+++ admin/pages/models/index.tsx
@@ -96,7 +96,7 @@ const Models = ({ initialModels, initialStatusCode, initialMessage, }:
               {notification.type === 'success' && <SuccessAlert>{notification.message}</SuccessAlert>}
             </>
           </TopCenteredSnackbar>
-        </Box> 
+        </Box>  
         <Loader open={loading} />
         <ModelsListSearchBar />
         <Box m='4vh' display='flex' justifyContent='center' >

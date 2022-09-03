89695fa2a698bdf506a6cce9c75040f81471cc03
exploitenomah <iksploiting@gmail.com>
2022-09-03 08:24:16 +0100

    remove unused import declarations

--- admin/components/layout/Layout.tsx
+++ admin/components/layout/Layout.tsx
@@ -1,9 +1,7 @@
 
 import AdminLayoutHeader from '@/components/header/Header'
 import Box from '@mui/material/Box'
-import { useContext } from 'react'
 import { LayoutProps } from '@/types/layout'
-import { UIContext } from '@/context/ui'
 import Head from './Head'
 import CustomizedBreadcrumbs from '@/components/common/breadcrumbs'
 import { useRouter } from 'next/router'

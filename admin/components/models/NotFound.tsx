731ee70d34faa4f5cf4b99c71be8ece87edf6cc3
exploitenomah <iksploiting@gmail.com>
2022-09-18 22:05:29 +0100

    switch from seperate ui context to reducer

--- admin/components/models/NotFound.tsx
+++ admin/components/models/NotFound.tsx
@@ -1,19 +1,17 @@
-import { ModelSearchContext } from "@/context/model.search"
-import { ModelsContext } from "@/context/models"
+import { Model } from "@/types/models"
 import { Typography } from "@mui/material"
-import { useContext } from "react"
 
 
 
-const NotFound = () => {
-
-  const { modelsDisplayed, models: stateModels, } = useContext(ModelsContext)
-  const { searchTerm } = useContext(ModelSearchContext)
+const NotFound = ({ searchTerm, models, initialStatusCode}: {
+  searchTerm: string, models: Model[], initialStatusCode: number
+}) => {
 
   return (
     <Typography variant='h1' >
-    {(searchTerm.trim().length > 0 && modelsDisplayed.length === 0) && 'No Models match your search'}
-    {searchTerm.trim().length === 0 && (stateModels.length === 0 || modelsDisplayed.length === 0) && 'Unable to fetch models. Please try again!'}
+    {(searchTerm.trim().length > 0 && models.length > 0) && 'No Models match your search'}
+    {searchTerm.trim().length === 0 && (models.length === 0) && initialStatusCode !== 200 && 'Unable to fetch models. Please try again!'}
+    {searchTerm.trim().length === 0 && (models.length === 0) && initialStatusCode === 200 && 'No Models.'}
   </Typography>
   )
 }

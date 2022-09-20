3901c82dc982b2dbe906838b9f0a1323f503108a
exploitenomah <iksploiting@gmail.com>
2022-09-20 08:48:00 +0100

    update login test

--- admin/cypress/e2e/login.cy.ts
+++ admin/cypress/e2e/login.cy.ts
@@ -3,13 +3,20 @@ const baseUrl = '/admin'
 
 describe('Login user with form', () => {
   it('Login user with email and password', () => {
+    cy.intercept(`${Cypress.env('CYPRESS_SERVER_URL')}/users/login`).as('login')
     cy.log(Cypress.env('CYPRESS_SERVER_URL'))
     cy.visit(baseUrl).location('href').should('contain', '/login')
 
     cy.get('[data-testid="email"]').type(Cypress.env('CYPRESS_TEST_USERNAME'))
     cy.get('[data-testid="password"]').type(Cypress.env('CYPRESS_TEST_PASSWORD'))
     cy.get('[data-testid="login"]').click()
-    cy.location('href').should('not.include', '/login')
+    cy.wait('@login').then(interception => {
+      if(interception.response.statusCode === 200){
+        cy.location('href').should('not.include', '/login')
+      }else{
+        cy.location('href').should('include', '/login')
+      }
+    })
   })
 })
 export {}

d03774df9f301683514abb0a593cf8898198e130
exploitenomah <iksploiting@gmail.com>
2022-09-20 07:26:05 +0100

    fix failing test by removing unnecessary wait for @login

--- admin/cypress/e2e/login.cy.ts
+++ admin/cypress/e2e/login.cy.ts
@@ -3,24 +3,13 @@ const baseUrl = '/admin'
 
 describe('Login user with form', () => {
   it('Login user with email and password', () => {
-    cy.intercept('POST', `${baseUrl}/api/login`,).as('login')
+    cy.log(Cypress.env('NEXT_PUBLIC_SERVER_URL'))
     cy.visit(baseUrl).location('href').should('contain', '/login')
 
     cy.get('[data-testid="email"]').type(Cypress.env('CYPRESS_TEST_USERNAME'))
     cy.get('[data-testid="password"]').type(Cypress.env('CYPRESS_TEST_PASSWORD'))
     cy.get('[data-testid="login"]').click()
-
-    cy.wait('@login').then((res) => {
-      if(res && res.response && res.response.body && res.response.body.token){
-        if(res.response.body.statusCode === 200){
-          cy.setCookie('access_token',  res.response.body.token)
-          cy.visit(baseUrl) 
-          cy.location('href').should('not.include', '/login')
-        }else {
-          cy.location('href').should('include', '/login')
-        }
-      }
-    })
+    cy.location('href').should('not.include', '/login')
   })
 })
 export {}

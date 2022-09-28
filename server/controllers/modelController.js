ee29fe82d28dfd0e6fd715b826a6f2f17c2a1f5d
exploitenomah <iksploiting@gmail.com>
2022-09-28 17:54:40 +0100

    refactor editmodel controller

--- server/controllers/modelController.js
+++ server/controllers/modelController.js
@@ -2,8 +2,6 @@
 const Model = require('../models/modelModel');
 const { asyncHelper } = require('../utils/asyncUtils')
 const {
-  editFields,
-  editDocument,
   handleDocDelete,  
   createCustomError,
   createDocument,
@@ -20,14 +18,40 @@ module.exports.createNewModel = asyncHelper(async (req, res, next) => {
   return next()
 })
 module.exports.editModelProfile = asyncHelper(async (req, res, next) => {
-  const model = req.doc
+  let model = await Model.findById(req.params.id)
   if(req.body.polaroids){
-    const fields = Object.keys(req.body.polaroids)
-    model.polaroids = editFields(model.polaroids, fields, req.body.polaroids)
-    delete req.body.polaroids 
-    await model.save()    
+    Object.keys(req.body.polaroids).forEach(pol => {
+      if(req.body.polaroids[pol]){
+        model.polaroids[pol] = req.body.polaroids[pol]
+      }
+    })
   }
-  editDocument([], model)(req, res, next)
+  if(req.body.extra_polaroids && req.body.extra_polaroids.length > 0){
+      let toUpdate
+      model.extra_polaroids.forEach(extra => {
+        toUpdate = req.body.extra_polaroids.find(el => el._id === extra._id.toString())
+        if(toUpdate){
+          Object.keys(toUpdate).map(el => {
+            extra[el] = toUpdate[el]
+          })
+        }
+      })
+      req.body.extra_polaroids.filter(item => item._id === undefined).forEach(el => {
+        model.extra_polaroids.push(el)
+      }) 
+   }
+  Object.keys(req.body).forEach(key => {
+    if(key !== 'polaroids' && key !== 'extra_polaroids'){
+      model[key] = req.body[key]
+    }
+  })
+  const saved = await model.save()
+  if (!saved) { return next(createCustomError('Unable to update profile! Please try again later', 500)) }
+  req.statusCode = 200
+  req.status = 'success'
+  req.message = 'Successfully updated!'
+  req.data = { doc: saved }
+  next()
 }) 
 module.exports.handleDelete =  handleDocDelete(Model, 'id')
 

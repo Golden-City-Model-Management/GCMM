4470459e8430796d89c1d328c297d015a07f50b1
exploitenomah <iksploiting@gmail.com>
2022-10-02 17:41:35 +0100

    save gallery document after creation

--- server/controllers/gallery.js
+++ server/controllers/gallery.js
@@ -9,9 +9,10 @@ module.exports.addImages = asyncHelper(async (req, res, next) => {
   if(!images || !Array.isArray(images) || images.length <= 0 ) return next(createCustomError('Please provide a list of images to add to the gallery!', 400))
   const docs = []
   for(const image of images){
-    docs.push(new GalleryImage(image))
+    docs.push(new GalleryImage(image).save())
   }
   const savedDocs = await Promise.all(docs)
+  
   req.statusCode = 201
   req.status = 'success'
   req.data = { images: savedDocs }

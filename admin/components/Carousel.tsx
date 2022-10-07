7dc15a1409217f498b600541fee29807e4ae3d48
exploitenomah <iksploiting@gmail.com>
2022-10-07 06:49:45 +0100

    change default export to refactored carousel

--- admin/components/Carousel.tsx
+++ admin/components/Carousel.tsx
@@ -7,21 +7,6 @@ export default function Carousel({ carouselItems, }: {
   carouselItems: any[]
 }) {
   return (
-    <Box overflow='hidden' position='absolute' height='100%' width='100%' >
-      <ReactMuiCarousel animation='slide' autoPlay={false} navButtonsAlwaysVisible
-        sx={{ height: '100%', overflow: 'hidden' }}
-        NextIcon={<ChevronRightOutlined color='secondary' />}
-        PrevIcon={<ChevronLeftOutlined color='secondary' />}>
-        {carouselItems.map((item, i) => <Box key={i}>{item}</Box>)}
-      </ReactMuiCarousel>
-    </Box>
-  );
-}
-
-export function RefactoredCarousel({ carouselItems, }: {
-  carouselItems: any[]
-}) {
-  return (
     <ReactMuiCarousel animation='slide' autoPlay={false} navButtonsAlwaysVisible
       NextIcon={<ChevronRightOutlined color='secondary' />}
       PrevIcon={<ChevronLeftOutlined color='secondary' />}>

9c2faac33156426367bbda8bf9b1075c827a3277
exploitenomah <iksploiting@gmail.com>
2022-10-09 11:31:50 +0100

    fix routing to admin

--- client/next.config.js
+++ client/next.config.js
@@ -2,18 +2,35 @@
 
 const ADMIN_URL =  'http://localhost:4000/'
 
-const nextConfig = {
-  reactStrictMode: true,
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return {
+//       fallback: [
+//         {
+//           source: '/admin',
+//           destination: process.env.ADMIN_URL || ADMIN_URL,
+//         },
+//       ]
+//     }
+//   },
+// }
+module.exports = {
   async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/:path*',
-          destination: process.env.ADMIN_URL || ADMIN_URL,
-        },
-      ]
-    }
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      {
+        source: '/admin',
+        destination: `${ADMIN_URL}/admin`,
+      },
+      {
+        source: '/admin/:path*',
+        destination: `${ADMIN_URL}/admin/:path*`,
+      },
+    ]
   },
 }
-
-module.exports = nextConfig
+// module.exports = nextConfig

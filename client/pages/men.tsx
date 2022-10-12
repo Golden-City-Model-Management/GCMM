e947c2223f6d5560fa46494d2b894fe751ac6685
exploitenomah <iksploiting@gmail.com>
2022-10-12 13:19:09 +0100

    basic markup for men pageg

--- client/pages/men.tsx
+++ client/pages/men.tsx
@@ -1,25 +1,39 @@
 
 
 import { NextPageWithLayout } from '@/types/pages'
-import LayoutOne from '@/components/layout/LayoutOne'
 import getLayout from '@/utils/pages/getLayout'
-import ComingSoonPlaceHolder from '@/components/common/ComingSoon'
-
+import LayoutTwo from '@/components/layout/LayoutTwo'
+import React from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography'
+import ModelCard from '@/components/models/ModelCard';
 
 const Men: NextPageWithLayout = () => {
-
   return (
-    <ComingSoonPlaceHolder page='Men' />
+    <Box component='article'> 
+      <Typography variant='caption' component='h1' textAlign='center' my={3} fontWeight='400'>
+        Men
+      </Typography>
+
+      <Box component='ul' display='flex' flexWrap='wrap' justifyContent='center' gap={3} padding={0} >
+        {
+          [1, 2,3,3,3,4,5,5,34,4,34, 1,2,2,2,22,2,2,22,2,2,2,22,2,2,2,2,2,2,22,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,,2,2,2,22,2,2,2,2222].map((e, i) => (
+            <ModelCard component='li' key={i} />
+          ))
+        }
+      </Box>
+    </Box>
   )
 }
 
 const props = {
-  title: 'Golden City Model Management | Main Board',
+  title: 'Golden City Model Management | Men',
   description: 'Golden City Model Management. Finding and refining talent. African Premium Agency located in Lagos, Nigeria',
-  favicon: '/vercel.svg',
+  favicon: '/logo.svg',
   pad: true,
+  canonical: `${process.env.BASE_URL}/men`
 }
 
-Men.getLayout = getLayout(LayoutOne, props)
+Men.getLayout = getLayout(LayoutTwo, props)
 
 export default Men

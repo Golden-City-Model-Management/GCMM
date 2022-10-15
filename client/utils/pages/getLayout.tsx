5faef98a0bf431777da1ca77a236af2bcc750eec
exploitenomah <iksploiting@gmail.com>
2022-10-15 17:13:09 +0100

    convert functions to named functions

--- client/utils/pages/getLayout.tsx
+++ client/utils/pages/getLayout.tsx
@@ -3,12 +3,12 @@ import { LayoutProps } from '@/types/layout'
 import { ReactElement, JSXElementConstructor } from 'react'
 
 
-export default function (
+export default function getLayout (
   Layout: (props: LayoutProps) => React.ReactElement,
   props: LayoutProps
 ) {
 
-  return (page: ReactElement<any, string | JSXElementConstructor<any>>) => {
+  return function PageWithLayout (page: ReactElement<any, string | JSXElementConstructor<any>>){
     return (
       <Layout {...props}>
         {page}
